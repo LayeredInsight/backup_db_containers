58ee0992a3e169a04e6044f33acd4a4b463a19ca
John Kinsella <jlk@layeredinsight.com>
2016-04-14 14:34:41 -0700

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # backup_mysql_containers
-Set of scripts to back up all MySQL containers on a Docker host
+This set of scripts will attempt to run a mysql backup on any
+container running on the local host that has "mysql" in it's container
+name. For each container, it will attempt to get a list of databases,
+and run mysql_dump against each one.
+
+This script has been designed to work with the (Offical MySQL Docker
+images)[https://hub.docker.com/_/mysql/], using the supported
+environment variable MYSQL_ROOT_PASSWORD to set the server's root
+password. In the future it may support other methods of determining
+mysql server authentication details.
+
+# Usage
+Packaging/install scripts will be created as requested...I guess
+this could be a privilged container, but personally that doesn't
+thrill me.
+
+Copy the two shell scripts here to somewhere on your docker host
+```
+cp *sh /usr/local/bin
+chmod 755 /usr/local/bin/
+```
+...and then review the constants set in each to see if they fit 
+your needs or need any modification.
+
+## Scheduling
+For the moment, the periodical execution of the backup scripts is
+left as an exercise for the reader. I'm currently using a cronjob
+on the docker host, but would rather something more centralized.
---
+++ backup_mysql_databases.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Backs up all mysql databases on a given host.
+#
+#  Copyright 2016 John Kinsella
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+###
+
+# This script expects $MYSQL_ROOT_PASSWORD to contain the root
+# password for the mysql server.
+
+MYSQLHOME=/var/lib/mysql
+MYSQLDUMP=/usr/bin/mysqldump
+MYSQLUSER=root
+DUMP_OPTIONS="-u $MYSQLUSER --password=$MYSQL_ROOT_PASSWORD --opt --compress --lock-tables"
+DATE=`date +%Y-%m-%d`
+
+if [ $# -gt 0 ]; then
+  BACKUP_DIR=$1
+else
+  echo "Usage: $0 <backup_directory>"
+fi
+
+DATABASES=`echo "show databases" | mysql --user=root --password=$MYSQL_ROOT_PASSWORD |tail --lines=+2`
+
+if [ ! -d $MYSQL_HOME ] ; then
+  echo "Could not find $MYSQL_HOME. Is mysql installed?"
+  exit 1
+fi
+
+mkdir -p $BACKUP_DIR
+for dbname in $DATABASES ; do
+  echo "Backing up $dbname"
+  $MYSQLDUMP $DUMP_OPTIONS $dbname > $BACKUP_DIR/$dbname.backup
+  gzip $BACKUP_DIR/$dbname.backup
+done
